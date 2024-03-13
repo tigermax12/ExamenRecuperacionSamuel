029c87cb417d96f64a1825483fdbd8f51ac9203a
SamuelPascual <daw08.2024@gmail.com>
2024-03-13 17:06:52 +0100

    Proycto documentado

--- Examen_2a/src/examen/Fecha.java
+++ Examen_2a/src/examen/Fecha.java
@@ -1,8 +1,21 @@
 package examen;
-
+/**
+ * Esta clase llamada fecha contiene en su interior un metodo y una variable. 
+ * @author Samuek Pascual
+ * @version 1.0
+ */
 public class Fecha {
+	/**
+	 * Esta variable almacena si la fecha es valida o no
+	 */
 	public static boolean fechaValida;
-
+	/**
+	 * Este metodo solicita 3 valores numericos  y devuelve un booleano 
+	 * @param anio Ingresas un numero 
+	 * @param mes Ingresas un numero 
+	 * @param dia Ingresas un numero 
+	 * @return boolean Devuelve true si la fecha es correcta y false si no es correcta 
+	 */
 	public static boolean validarFecha(int anio, int mes, int dia) {
 		fechaValida = false;
 		if ((dia > 0) && (mes > 0) && (anio > 0)) {
